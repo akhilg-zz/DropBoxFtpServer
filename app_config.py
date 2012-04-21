5eb81978808ef35f62f782326035067a41ed23ad
Akhil Gupta <akhilg@ygmail.com>
2012-04-21 02:57:37 -0700

    Add a config file to hold the app key and secret

---
+++ app_config.py
@@ -0,0 +1,6 @@
+# Get your app key and secret from the Dropbox developer website
+APP_KEY = '6qedtlyqcwt7izw'
+APP_SECRET = 'ny4j2qfh2rpmjii'
+
+# ACCESS_TYPE should be 'dropbox' or 'app_folder' as configured for your app
+ACCESS_TYPE = 'app_folder'
