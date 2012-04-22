9a2c00955a704834cf519006102f0a33896b0e34
Akhil Gupta <akhilg@ygmail.com>
2012-04-22 00:29:03 -0700

    Fix a couple of minor fixes. Remove function main from ftpserver.py

--- ftpserver.py
+++ ftpserver.py
@@ -265,14 +265,7 @@ if not hasattr(os, 'chmod'):
     del proto_cmds['SITE CHMOD']
 
 
-# A wrapper around os.strerror() which may be not available
-# on all platforms (e.g. pythonCE). Expected arg is a
-# EnvironmentError or derived class instance.
-if hasattr(os, 'strerror'):
-    _strerror = lambda err: os.strerror(err.errno)
-else:
-    _strerror = lambda err: err.strerror
-
+_strerror = lambda err: str(err)
 
 class _Scheduler(object):
     """Run the scheduled functions due to expire soonest (if any)."""
@@ -2380,7 +2373,7 @@ class FTPHandler(object, AsyncChat):
         io_stream = io.FileIO(file)
         buffered_reader = io.BufferedReader(io_stream)
         self.fs.put_file(file, buffered_reader)
-        buffered.close()
+        buffered_reader.close()
         line = "Finished pushing %s to dropbox at %s" % (
             file, time.strftime("%H:%M:%S", time.localtime(time.time())))
         self.log(line)
@@ -3817,72 +3810,3 @@ class FTPServer(object, Acceptor):
                     raise
         del _scheduler._tasks[:]
 
-
-def main():
-    """Start a stand alone anonymous FTP server."""
-
-    class CustomizedOptionFormatter(optparse.IndentedHelpFormatter):
-        """Formats options shown in help in a prettier way."""
-
-        def format_option(self, option):
-            result = []
-            opts = self.option_strings[option]
-            result.append('  %s\n' % opts)
-            if option.help:
-                help_text = '     %s\n\n' % self.expand_default(option)
-                result.append(help_text)
-            return ''.join(result)
-
-    usage = "python -m pyftpdlib.ftpserver [options]"
-    parser = optparse.OptionParser(usage=usage, description=main.__doc__,
-                                   formatter=CustomizedOptionFormatter())
-    parser.add_option('-i', '--interface', default='0.0.0.0', metavar="ADDRESS",
-                      help="specify the interface to run on (default all "
-                           "interfaces)")
-    parser.add_option('-p', '--port', type="int", default=21, metavar="PORT",
-                      help="specity port number to run on (default 21)")
-    parser.add_option('-w', '--write', action="store_true", default=False,
-                      help="grants write access for the anonymous user "
-                           "(default read-only)")
-    parser.add_option('-d', '--directory', default=os.getcwd(), metavar="FOLDER",
-                      help="specify the directory to share (default current "
-                           "directory)")
-    parser.add_option('-n', '--nat-address', default=None, metavar="ADDRESS",
-                      help="the NAT address to use for passive connections")
-    parser.add_option('-r', '--range', default=None, metavar="FROM-TO",
-                      help="the range of TCP ports to use for passive "
-                           "connections (e.g. -r 8000-9000)")
-    parser.add_option('-v', '--version', action='store_true',
-                      help="print pyftpdlib version and exit")
-
-    options, args = parser.parse_args()
-    if options.version:
-        sys.exit("pyftpdlib %s" % __ver__)
-    passive_ports = None
-    if options.range:
-        try:
-            start, stop = options.range.split('-')
-            start = int(start)
-            stop = int(stop)
-        except ValueError:
-            parser.error('invalid argument passed to -r option')
-        else:
-            passive_ports = range(start, stop + 1)
-    # On recent Windows versions, if address is not specified and IPv6
-    # is installed the socket will listen on IPv6 by default; in this
-    # case we force IPv4 instead.
-    if os.name in ('nt', 'ce') and not options.interface:
-        options.interface = '0.0.0.0'
-
-    authorizer = DummyAuthorizer()
-    perm = options.write and "elradfmwM" or "elr"
-    authorizer.add_anonymous(options.directory, perm=perm)
-    handler = FTPHandler
-    handler.authorizer = authorizer
-    handler.masquerade_address = options.nat_address
-    handler.passive_ports = passive_ports
-    ftpd = FTPServer((options.interface, options.port), FTPHandler)
-    ftpd.serve_forever()
-
-if __name__ == '__main__':
-    main()
