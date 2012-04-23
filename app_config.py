9c503fb432b32b2db94d58ebd638c372996969db
Akhil Gupta <akhilg@ygmail.com>
2012-04-23 02:26:29 -0700

    Reduce the number of connections allowed to prevent overload.

--- app_config.py
+++ app_config.py
@@ -14,9 +14,9 @@ HTTP_SERVER_PORT = 8888
 FTP_SERVER_PORT = 21
 
 # Configuration that controls the scalability.
-MAX_CONNECTIONS = 10000
-MAX_CONNECTIONS_PER_IP = 10000
+MAX_CONNECTIONS = 1000
+MAX_CONNECTIONS_PER_IP = 1000
 # Since the python API to put files in dropbox is synchronous and
 # takes about 15-20 seconds for a 10MB file, we use a thread pool to
 # make sure we don't block the IOLoop thread for too long.
-MAX_UPLOAD_TO_DROPBOX = 20
+MAX_UPLOAD_TO_DROPBOX = 50
